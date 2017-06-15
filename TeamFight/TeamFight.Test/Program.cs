6b5f7534b4d2c0bbb6d87e1ba34399b13385643b
CHENBAIYU <chenbaiyu@moqihuyu.com>
2017-06-15 13:55:54 +0800

    add test code

--- TeamFight/TeamFight.Test/Program.cs
+++ TeamFight/TeamFight.Test/Program.cs
@@ -14,8 +14,8 @@ namespace TeamFight.Test
             var baseurl = "http://localhost:9955/api/teamservice";
             var encoding = Encoding.UTF8;
             int charId;
-            Guid teamId;
-            Guid inviteTeamId;
+            Guid teamId = Guid.Empty;
+            Guid inviteTeamId = Guid.Empty;
 
             Console.WriteLine("请输入一个角色Id，后续流程将会使用该Id的角色");
             Console.WriteLine(int.TryParse(Console.ReadLine(), out charId) ? "记录成功，请输入后续指令" : "角色Id错误，请关闭程序重试");
@@ -30,89 +30,107 @@ namespace TeamFight.Test
                     switch (input)
                     {
                         case "login":
-                        {
-                            var url = baseurl + "/login";
-                            var postData = encoding.GetBytes("CharId=" + charId);
-                            var responseData = client.UploadData(url, "POST", postData);
-                            Console.WriteLine(encoding.GetString(responseData));
-                        }
+                            {
+                                var url = baseurl + "/login";
+                                var postData = encoding.GetBytes("CharId=" + charId);
+                                var responseData = client.UploadData(url, "POST", postData);
+                                Console.WriteLine(encoding.GetString(responseData));
+                            }
                             break;
                         case "create":
-                        {
-                            var url = baseurl + "/create";
-                            var postData = encoding.GetBytes("CharId=" + charId);
-                            var responseData = client.UploadData(url, "POST", postData);
-                            teamId = JsonConvert.DeserializeObject<Guid>(encoding.GetString(responseData));
-                            Console.WriteLine("已创建组队：队伍Id[" + teamId + "]");
-                        }
+                            {
+                                var url = baseurl + "/create";
+                                var postData = encoding.GetBytes("CharId=" + charId);
+                                var responseData = client.UploadData(url, "POST", postData);
+                                teamId = JsonConvert.DeserializeObject<Guid>(encoding.GetString(responseData));
+                                Console.WriteLine("已创建组队：队伍Id[" + teamId + "]");
+                            }
                             break;
                         case "pullfriend":
-                        {
-                            var url = baseurl + "/PullFriendList";
-                            var postData = encoding.GetBytes("CharId=" + charId);
-                            var responseData = client.UploadData(url, "POST", postData);
-                            var friendIdList = JsonConvert.DeserializeObject<List<int>>(encoding.GetString(responseData));
-
-                            if (friendIdList == null)
                             {
-                                Console.WriteLine("好友列表为空");
-                            }
-                            else
-                            {
-                                var sb = new StringBuilder();
-                                foreach (var id in friendIdList)
-                                    sb.Append(id + ",");
+                                var url = baseurl + "/PullFriendList";
+                                var postData = encoding.GetBytes("CharId=" + charId);
+                                var responseData = client.UploadData(url, "POST", postData);
+                                var friendIdList = JsonConvert.DeserializeObject<List<int>>(encoding.GetString(responseData));
 
-                                Console.WriteLine("已获取好友列表，好友Id为 " + sb);
+                                if (friendIdList == null)
+                                {
+                                    Console.WriteLine("好友列表为空");
+                                }
+                                else
+                                {
+                                    var sb = new StringBuilder();
+                                    foreach (var id in friendIdList)
+                                        sb.Append(id + ",");
+
+                                    Console.WriteLine("已获取好友列表，好友Id为 " + sb);
+                                }
                             }
-                        }
                             break;
                         case "pullrecommandteams":
-                        {
-                            var url = baseurl + "/PullRecommendTeams";
-                            byte[] postData = {};
-                            var responseData = client.UploadData(url, "POST", postData);
-
-                            var teamList =
-                                JsonConvert.DeserializeObject<List<RecommendTeamModel>>(encoding.GetString(responseData));
-
-                            if (teamList == null)
                             {
-                                Console.WriteLine("没有推荐组队");
-                            }
-                            else
-                            {
-                                var sb = new StringBuilder();
-                                foreach (var model in teamList)
-                                    sb.AppendFormat("队伍Id：{0}，队长名字：{1}，队长等级{2}，队长头像Id：{3}\n", model.TeamId,
-                                        model.CaptainName, model.CaptainLevel, model.CaptainFaceId);
+                                var url = baseurl + "/PullRecommendTeams";
+                                byte[] postData = { };
+                                var responseData = client.UploadData(url, "POST", postData);
 
-                                Console.WriteLine("已组队推荐列表，内容 " + sb);
+                                var teamList =
+                                    JsonConvert.DeserializeObject<List<RecommendTeamModel>>(encoding.GetString(responseData));
+
+                                if (teamList == null)
+                                {
+                                    Console.WriteLine("没有推荐组队");
+                                }
+                                else
+                                {
+                                    var sb = new StringBuilder();
+                                    foreach (var model in teamList)
+                                        sb.AppendFormat("队伍Id：{0}，队长名字：{1}，队长等级{2}，队长头像Id：{3}\n", model.TeamId,
+                                            model.CaptainName, model.CaptainLevel, model.CaptainFaceId);
+
+                                    Console.WriteLine("已组队推荐列表，内容 " + sb);
+                                }
                             }
-                        }
                             break;
                         case "invite":
-                        {
-                            Console.WriteLine("请输入要邀请的角色的Id：");
-                            var invitecharId = Console.ReadLine();
+                            {
+                                Console.WriteLine("请输入要邀请的角色的Id：");
+                                var invitecharId = Console.ReadLine();
 
-                            var url = baseurl + "/InviteCharacter";
-                            var postData = encoding.GetBytes("CharId=" + charId + "&InvitedCharId=" + invitecharId);
-                            var responseData = client.UploadData(url, "POST", postData);
+                                var url = baseurl + "/InviteCharacter";
+                                var postData = encoding.GetBytes("CharId=" + charId + "&InvitedCharId=" + invitecharId);
+                                var responseData = client.UploadData(url, "POST", postData);
 
-                            var result = JsonConvert.DeserializeObject<bool>(encoding.GetString(responseData));
-                            Console.WriteLine(result ? "发出邀请成功" : "发出邀请失败");
-                        }
+                                var result = JsonConvert.DeserializeObject<bool>(encoding.GetString(responseData));
+                                Console.WriteLine(result ? "发出邀请成功" : "发出邀请失败");
+                            }
                             break;
                         case "queryinvitation":
-                        {
-                            var url = baseurl + "/QueryInvitation";
-                            var postData = encoding.GetBytes("CharId=" + charId);
-                            var responseData = client.UploadData(url, "POST", postData);
+                            {
+                                var url = baseurl + "/QueryInvitation";
+                                var postData = encoding.GetBytes("CharId=" + charId);
+                                var responseData = client.UploadData(url, "POST", postData);
 
-                            inviteTeamId = JsonConvert.DeserializeObject<Guid>(encoding.GetString(responseData));
-                            Console.WriteLine(inviteTeamId != Guid.Empty ? "有组队邀请，队伍Id " + inviteTeamId : "无组队邀请");
-                        }
+                                inviteTeamId = JsonConvert.DeserializeObject<Guid>(encoding.GetString(responseData));
+                                Console.WriteLine(inviteTeamId != Guid.Empty ? "有组队邀请，队伍Id " + inviteTeamId : "无组队邀请");
+                            }
+                            break;
+                        case "jointeam":
+                            {
+                                if (inviteTeamId == Guid.Empty)
+                                {
+                                    Console.WriteLine("还没有接受到组队邀请");
+                                }
+                                else
+                                {
+                                    var url = baseurl + "/JoinTeam";
+                                    var postData =
+                                        encoding.GetBytes("TeamId=" + inviteTeamId + "&InvitedCharacterId=" + charId);
+                                    var responseData = client.UploadData(url, "POST", postData);
+
+                                    var result = JsonConvert.DeserializeObject<bool>(encoding.GetString(responseData));
+                                    Console.WriteLine(result ? "发出邀请成功" : "发出邀请失败");
+                                }
+                            }
                             break;
                     }
                 }
